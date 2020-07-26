46e76a0c4d750cdf2a48ee15bddcc935b2f0f040
andres0599 <andres1a2b3c2@gmail.com>
2020-07-25 20:24:18 -0600

    A partir de una lista de paginas, entrar a todas y recolectar datos guardandolos en un csv

--- crawler.js
+++ crawler.js
@@ -2,36 +2,39 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 
 const fs = require('fs-extra');
-const writeStream = fs.createWriteStream('qutes.csv');
+const writeStream = fs.createWriteStream('titulos.csv');
 
 async function init() {
+    let paginas = ['https://es.wikipedia.org/wiki/Python','https://en.wikipedia.org/wiki/C_(programming_language)','https://en.wikipedia.org/wiki/Java_(programming_language)']
+    for(i=0;i<paginas.length;i++){
     try {
         const $ = await request({
-            uri: 'http://quotes.toscrape.com/',
+            uri: paginas[i],
             transform: body => cheerio.load(body)
         });
 
-        const websiteTitle = $('title');
-        console.log('Title: ', websiteTitle.html());
+        const websiteTitle = $('title').html();
+        console.log('Title: ', websiteTitle);
 
-        const webSiteHeading = $('h1')
-        console.log('Heading: ', webSiteHeading.text().trim());
+        const webSiteHeading = $('h1').text().trim();
+        console.log('Heading: ', webSiteHeading);
+        writeStream.write(`${websiteTitle},${webSiteHeading}\n`);
 
-        const quote = $('.quote').find('a');
-        console.log(quote.html());
+        //const quote = $('.quote').find('a');
+        //console.log(quote.html());
 
-        const third_quote = $('.quote').next().next();
+        //const third_quote = $('.quote').next().next();
         // console.log(third_quote.html())
 
         // Parent
-        const containerClass = $('.row.header-box');
+        //const containerClass = $('.row.header-box');
         // console.log(containerClass.parent().html())
 
         // $('.quote span.text').each((i, el) => {
         //     const quote_text = $(el).text();
         //     const quote = quote_text.replace(/(^\“|\”$)/g, "");
         // })        
-
+/*
         writeStream.write('Quote|Author|Tags\n');
         const tags = [];
         $('.quote').each((i, el) => {
@@ -43,8 +46,8 @@ async function init() {
             writeStream.write(`${text}|${author}|${tags}\n`);
             // console.log(i, text, author)
         })
-
-        console.log('Done.');
+*/
+        //console.log('Done.');
         // $('.quote .tags a').each((i, el) => {
         //     // console.log($(el).html())
         //     const text = $(el).text();
@@ -55,6 +58,7 @@ async function init() {
     } catch (e) {
         console.log(e);
     }
+}
 }
 
 init();
