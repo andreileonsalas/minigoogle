fe2c96e75b3be2aa027a6f5d8b13c34f132811b9
otoniel <000otoniel000@gmail.com>
2020-08-02 23:31:52 -0600

    agregado algunas comprobaciones al crawler
    se agregaron algunas comprobaciones al crawler

--- crawler.js
+++ crawler.js
@@ -4,10 +4,11 @@ const cheerio = require('cheerio');
 const fs = require('fs-extra');
 const { accessSync } = require('fs-extra');
 const writeStream = fs.createWriteStream('titulos.txt');
+const evitar = ["pdf","PDF"];
 
 async function init() {
-    let paginas = ['https://www.ticportal.es/glosario-tic/base-datos-database']
-    for(i=0;i<1;i++){
+    let paginas = ['https://medium.com/javascript-scene/the-forgotten-history-of-oop-88d71b9b2d9f#:~:text=The%20big%20idea%20is%20messaging.%E2%80%9D&text=In%20a%202003%20email%20exchange,%2Dbinding%20of%20all%20things.%E2%80%9D']
+    for(i=0;i < paginas.length && i<20;i++){
     try {
         const $ = await request({
             uri: paginas[i],
@@ -15,7 +16,9 @@ async function init() {
         });
 
         writeStream.write(`Titulo\n`);
-        const websiteTitle = $('title').html().trim();
+        var websiteTitle = ""
+        if ($('title').html())
+            websiteTitle = $('title').html().trim();
         writeStream.write(`${websiteTitle}\n`);
 
         writeStream.write(`h1\n`);
@@ -47,10 +50,13 @@ async function init() {
             $(this).find('a').each(function() {
                 let enlace =$(this).attr('href');
                 writeStream.write(`${enlace}\n`);
-                paginas.push(enlace);
+                if (enlace != undefined)
+                    enlacex = completar_link(enlace,paginas[i])
+                if (verificar_link(evitar,enlace))
+                    paginas.push(enlacex);
             })
         })
-
+console.log(paginas);
     } catch (e) {
         console.log(e);
     }
@@ -58,6 +64,42 @@ async function init() {
 }
 
 
+function verificar_link (lista,link){
+    result = true;
+    if (link != undefined)
+        lista.forEach(element => {
+            if( link.indexOf(element) > -1){
+                result = false;
+            }
+        });    
+    return result;
+}
+
+function revisar_inicio (link){
+    result = false;
+    inicio = link.substr(0,4)
+    parte = "http";
+    if (inicio == parte){
+        result = true;
+    }
+
+    return result;
+}
+
+function completar_link (link,linkInicial){
+    if (revisar_inicio(link)){
+        return link;
+    }
+    else{
+        newlink = linkInicial.substr(0,linkInicial.indexOf("/",9))
+        if(link[0] == "/")
+            return newlink +  link;
+        else
+            return newlink + "/" + link;
+    }
+}
+
 
 
 init();
+
