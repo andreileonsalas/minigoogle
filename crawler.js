983e252eaae356ac66a8c01c41da2006a19222a7
andres0599 <andres1a2b3c2@gmail.com>
2020-08-05 10:24:03 -0600

    formato

--- crawler.js
+++ crawler.js
@@ -20,7 +20,7 @@ function rep(l,e){
 
 async function init() {
     let paginas = ['https://es.wikipedia.org/wiki/Real_Madrid_Club_de_F%C3%BAtbol','https://es.wikipedia.org/wiki/Segunda_Guerra_Mundial','https://es.wikipedia.org/wiki/Pol%C3%ADtica', 'https://es.wikipedia.org/wiki/COVID-19' ]
-    for(i=0;i < paginas.length && i<20;i++){
+    for(i=0;i < paginas.length && i<3;i++){
         const writeStream = fs.createWriteStream('pagina'+(i+1)+'.txt');
     try {
         const $ = await request({
@@ -28,41 +28,42 @@ async function init() {
             transform: body => cheerio.load(body)
         });
 
-        writeStream.write(`Titulo\n`);
         var websiteTitle = ""
         if ($('title').html())
             websiteTitle = $('title').html().trim();
-        writeStream.write(`${websiteTitle}\n`);
+        writeStream.write(`Titulo|${websiteTitle}\n`);
 
-        writeStream.write(`h1\n`);
         $('h1').each(function(){
             const h1 = $(this).text().trim();
-            writeStream.write(`${h1}\n`);
+            writeStream.write(`h1|${h1}\n`);
         })
 
-        writeStream.write(`h2\n`);
         $('h2').each(function(){
             const h2 = $(this).text().trim();
-            writeStream.write(`${h2}\n`);
+            writeStream.write(`h2|${h2}\n`);
         })
 
-        writeStream.write(`h3\n`);
         $('h3').each(function(){
             const h3 = $(this).text().trim();
-            writeStream.write(`${h3}\n`);
+            writeStream.write(`h3|${h3}\n`);
         })
 
-        writeStream.write(`p\n`);
+        $('h4').each(function(){
+            const h4 = $(this).text().trim();
+            writeStream.write(`h4|${h4}\n`);
+        })
+
+
         $('p').each(function(){
             const p = $(this).text();
-            writeStream.write(`${p}\n`);
+            writeStream.write(`p|${p}\n`);
         })
         
-        writeStream.write(`href\n`);
+
         $('p').each(function() {
             $(this).find('a').each(function() {
                 let enlace =$(this).attr('href');
-                writeStream.write(`${enlace}\n`);
+                writeStream.write(`href|${enlace}\n`);
                 if (enlace != undefined)
                     enlacex = completar_link(enlace,paginas[i])
                 if (verificar_link(evitar,enlace))
