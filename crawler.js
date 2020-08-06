c106e9e38f0aeeb9b9a85d2caf2ac7b8c88e236a
andres0599 <andres1a2b3c2@gmail.com>
2020-08-06 10:36:50 -0600

    profundidad arreglada, todo en un mismo documento

--- crawler.js
+++ crawler.js
@@ -19,67 +19,73 @@ function rep(l,e){
 }
 
 async function init() {
-    let paginas = ['https://es.wikipedia.org/wiki/Real_Madrid_Club_de_F%C3%BAtbol','https://es.wikipedia.org/wiki/Segunda_Guerra_Mundial','https://es.wikipedia.org/wiki/Pol%C3%ADtica', 'https://es.wikipedia.org/wiki/COVID-19' ]
-    for(i=0;i < paginas.length && i<6;i++){
-        const writeStream = fs.createWriteStream('pagina'+(i+1)+'.txt');
-    try {
-        const $ = await request({
-            uri: paginas[i],
-            transform: body => cheerio.load(body)
-        });
-        writeStream.write('href|'+paginas[i]+"\n");
-        var websiteTitle = ""
-        if ($('title').html())
-            websiteTitle = $('title').html().trim();
-            writeStream.write(`Titulo|${websiteTitle}\n`);
-            writeStream_all.write(`Titulo|${websiteTitle}\n`);
+    let paginas = [['https://es.wikipedia.org/wiki/Real_Madrid_Club_de_F%C3%BAtbol','https://es.wikipedia.org/wiki/Segunda_Guerra_Mundial','https://es.wikipedia.org/wiki/Pol%C3%ADtica', 'https://es.wikipedia.org/wiki/COVID-19']]
+    for(j=0;j<paginas.length && j<5; j++){
+        let band = 0
+        for(i=0;i < paginas[j].length && i<5;i++){
+            console.log(i);
+        try {
+            const $ = await request({
+                uri: paginas[j][i],
+                transform: body => cheerio.load(body)
+            });
 
-        $('h1').each(function(){
-            const h1 = $(this).text().trim();
-            writeStream.write(`h1|${h1}\n`);
-            writeStream_all.write(`h1|${h1}\n`);
-        })
+            writeStream_all.write(`href|${paginas[j][i]}\n`);
+            var websiteTitle = ""
+            if ($('title').html())
+                websiteTitle = $('title').html().trim();
+                writeStream_all.write(`Titulo|${websiteTitle}\n`);
 
-        $('h2').each(function(){
-            const h2 = $(this).text().trim();
-            writeStream.write(`h2|${h2}\n`);
-            writeStream_all.write(`h2|${h2}\n`);
-        })
-
-        $('h3').each(function(){
-            const h3 = $(this).text().trim();
-            writeStream.write(`h3|${h3}\n`);
-            writeStream_all.write(`h3|${h3}\n`);
-        })
-
-        $('h4').each(function(){
-            const h4 = $(this).text().trim();
-            writeStream.write(`h4|${h4}\n`);
-            writeStream_all.write(`h4|${h4}\n`);
-        })
-
-        $('p').each(function(){
-            const p = $(this).text();
-            writeStream.write(`p|${p}\n`);
-            writeStream_all.write(`p|${p}\n`);
-        })
-        
-        $('p').each(function() {
-            $(this).find('a').each(function() {
-                let enlace =$(this).attr('href');
-                //writeStream.write(`href|${enlace}\n`);
-                //writeStream_all.write(`href|${enlace}\n`);
-                if (enlace != undefined){
-                    enlacex = completar_link(enlace,paginas[i])
-                if (verificar_link(evitar,enlace))
-                    paginas.push(enlacex);
-                }
+            $('h1').each(function(){
+                const h1 = $(this).text().trim();
+                writeStream_all.write(`h1|${h1}\n`);
             })
-        })
-console.log(paginas);
-    } catch (e) {
-        console.log(e);
-    }
+
+            $('h2').each(function(){
+                const h2 = $(this).text().trim();
+                writeStream_all.write(`h2|${h2}\n`);
+            })
+
+            $('h3').each(function(){
+                const h3 = $(this).text().trim();
+                writeStream_all.write(`h3|${h3}\n`);
+            })
+
+            $('h4').each(function(){
+                const h4 = $(this).text().trim();
+                writeStream_all.write(`h4|${h4}\n`);
+            })
+
+
+            $('p').each(function(){
+                const p = $(this).text();
+                writeStream_all.write(`p|${p}\n`);
+            })
+            
+
+            $('p').each(function() {
+                $(this).find('a').each(function() {
+                    let enlace =$(this).attr('href');
+                    if (enlace != undefined)
+                        enlacex = completar_link(enlace,paginas[j][i])
+                    if (verificar_link(evitar,enlace))
+                    if (band==0){
+                        paginas.push([enlacex]);
+                        band = 1
+                    }
+                    if (band == 1){
+                        paginas[j+1].push(enlacex);
+                    }
+                   
+                        
+                })
+            })
+    
+        } catch (e) {
+            console.log(e);
+        }
+}
+
 }
 }
 
@@ -111,29 +117,7 @@ function completar_link (link,linkInicial){
         return link;
     }
     else{
-       //console.log(link)
-       //console.log(linkInicial)
-        
-        pos =  link.indexOf("/",1);
-        partelink = link.substr(0,pos);
-        //console.log(partelink );
-        newlink = "";
-
-        if (partelink.length == 0){
-            newlink = linkInicial.substr(0,linkInicial.indexOf("/",9))
-            //console.log("entro al 1 ")
-        }
-        else{
-          //  console.log("entro al segundo alt e")
-            pos2 = linkInicial.indexOf(partelink,0)
-            if (pos2 == -1){
-                newlink = linkInicial.substr(0,linkInicial.indexOf("/",9))
-            }
-            else{
-                newlink = linkInicial.substr(0,pos2)
-            }
-        }
-        //console.log("la parte del link es: " + newlink + "\n");
+        newlink = linkInicial.substr(0,linkInicial.indexOf("/",9))
         if(link[0] == "/")
             return newlink +  link;
         else
